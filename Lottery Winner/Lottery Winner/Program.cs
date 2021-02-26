edca650b2474c938702c3832ed504a8f9be7e10c
Yauchin Lam <yauchin.lam@outlook.com>
2021-02-25 20:53:48 -0800

    GitHub Test
    Ok GitHub works

--- Lottery Winner/Lottery Winner/Program.cs
+++ Lottery Winner/Lottery Winner/Program.cs
@@ -6,6 +6,7 @@ namespace Lottery_Winner
     {
         static void Main(string[] args)
         {
+            //Add a new comment to test github
             Random r = new Random();
             int winNum = r.Next(0, 100);
             Console.WriteLine("Guess a number between 1 and 100 and see if you won the lottery?");
