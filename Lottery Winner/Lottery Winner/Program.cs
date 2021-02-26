0ee68cde631d79d3b1f42d50c3480033e890ea82
Yauchin Lam <yauchin.lam@outlook.com>
2021-02-25 20:50:43 -0800

    The actual files
    I already made the project and i added it to this one

---
+++ Lottery Winner/Lottery Winner/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lottery_Winner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int winNum = r.Next(0, 100);
+            Console.WriteLine("Guess a number between 1 and 100 and see if you won the lottery?");
+            string num = Console.ReadLine();
+            double w = Convert.ToDouble(num);
+            if (w==winNum)
+                Console.WriteLine("Congratulations you won!");
+            else
+                Console.WriteLine("Better Luck next time!");
+        }
+    }
+}
